7fc6f40a0681480eff93172e10d21c3f988be0d6
jasonpieroni <jasonpieroni@gmail.com>
2023-05-31 15:11:34 -0400

    added files to main branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setMessage(`Welcome ${name}!`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Welcome, Enter Name Below!</h1>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor='name'>Name:</label>
+      <input 
+        type='text' 
+        name='name' 
+        id='name' 
+        value={name} 
+        placeholder='Jimmy Butler' 
+        onChange={handleChange} />
+      <button type='submit'>Submit</button>
+    </form>
+    <p>{message}</p>
     </div>
   );
 }
